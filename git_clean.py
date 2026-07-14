5cbed448d88a5c0f1fc9f77ffbf2e7bdea83992e
dongfangzhizhu <axu0606@gmail.com>
2026-07-14 14:32:56 +0800

    清理临时文件

--- git_clean.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-r = subprocess.run(['git', 'add', '-A'], cwd=r'D:\SouceCode\python2net\SAMALL\SAM_TorchSharp', capture_output=True, text=True)
-r2 = subprocess.run(['git', 'commit', '-m', '清理临时构建脚本'], cwd=r'D:\SouceCode\python2net\SAMALL\SAM_TorchSharp', capture_output=True, text=True)
-print(r2.stdout[:300])
